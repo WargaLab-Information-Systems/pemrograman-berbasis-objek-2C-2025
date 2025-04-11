6a7d2e897ba36f5c213ead2896d0fc0719d43bdd
unknown <aaminuddin81@gmail.com>
2025-04-11 07:50:33 +0700

    240441100018_MAminuddinMarhan_Modul1_AsprakKakYusri

---
+++ modul-1/240441100018-MAminuddinMarhan/modul1_soal2.py
@@ -0,0 +1,28 @@
+class mahasiswa:
+    def __init__(self):
+        self.nama = ""
+        self.nim = ""
+        self.prodi = ""
+        self.alamat = ""
+    def input_mahasiswa(self):
+        print("-----INPUT DATA MAHASISWA-----")
+        self.nama = input("Masukkan Nama Mahasiswa         : ")
+        self.nim = input("Masukkan NIM Mahasiswa          : ")
+        self.prodi = input("Masukkan Prodi/Jurusan Mahasiswa: ")
+        self.alamat = input("Masukkan Alamat Mahasiswa       : ")
+        print()
+    def tampilkan_mahasiswa(self):
+        print("\n---DATA MAHASISWA---")
+        print(f"Nama Mahasiswa  : {self.nama}")
+        print(f"NIM Mahasiswa   : {self.nim}")
+        print(f"Prodi Mahasiswa : {self.prodi}")
+        print(f"Alamat Mahasiswa: {self.alamat}")
+mhs1 = mahasiswa()
+mhs2 = mahasiswa()
+mhs3 = mahasiswa()
+mhs1.input_mahasiswa()
+mhs2.input_mahasiswa()
+mhs3.input_mahasiswa()
+mhs1.tampilkan_mahasiswa()
+mhs2.tampilkan_mahasiswa()
+mhs3.tampilkan_mahasiswa()
