6cbb73a8da529983b17ce0b4a2fd4fd42268fc24
dafazuhdi <dafasanjaya7727@gmail.com>
2025-05-05 14:59:34 +0700

    240441100112-ZUHDI MUZHAFFAR SANJAYA-MUHAMMAD MAULANA KHANIF-MODUL 3

---
+++ modul-3/240441100112-ZUHDI MUZHAFFAR SANJAYA/soal 1.py
@@ -0,0 +1,61 @@
+class Karyawan:
+    def __init__(self, nama, gaji, departemen):
+        self.nama = nama
+        self.gaji = gaji
+        self.departemen = departemen
+
+    def info(self):
+        print(f"Nama: {self.nama}")
+        print(f"Gaji: {self.gaji}")
+        print(f"Departemen: {self.departemen}")
+
+class KaryawanTetap(Karyawan):
+    def __init__(self, nama, gaji, departemen, tunjangan):
+        super().__init__(nama, gaji, departemen)
+        self.tunjangan = tunjangan
+
+    def info(self):
+        super().info()
+        print(f"Tunjangan: {self.tunjangan}")
+        print("Status: Karyawan Tetap\n")
+
+class KaryawanHarian(Karyawan):
+    def __init__(self, nama, gaji, departemen, jam_kerja):
+        super().__init__(nama, gaji, departemen)
+        self.jam_kerja = jam_kerja
+
+    def info(self):
+        super().info()
+        print(f"Jam Kerja per Hari: {self.jam_kerja}")
+        print("Status: Karyawan Harian\n")
+
+class ManajemenKaryawan:
+    def __init__(self):
+        self.daftar_karyawan = []
+
+    def tambah_karyawan(self, karyawan):
+        self.daftar_karyawan.append(karyawan)
+
+    def tampilkan_semua_karyawan(self):
+        print("=== Daftar Semua Karyawan ===")
+        for karyawan in self.daftar_karyawan:
+            karyawan.info()
+
+# Contoh penggunaan
+manajemen = ManajemenKaryawan()
+
+# Menambahkan beberapa karyawan tetap
+karyawan1 = KaryawanTetap("Rudy", 500000, "Keuangan", 100000)
+karyawan2 = KaryawanTetap("Bian", 550000, "IT", 120000)
+
+# Menambahkan beberapa karyawan harian
+karyawan3 = KaryawanHarian("Salsa", 20000, "Gudang", 8)
+karyawan4 = KaryawanHarian("Tika", 18000, "Produksi", 7)
+
+# Menambahkan ke dalam manajemen
+manajemen.tambah_karyawan(karyawan1)
+manajemen.tambah_karyawan(karyawan2)
+manajemen.tambah_karyawan(karyawan3)
+manajemen.tambah_karyawan(karyawan4)
+
+manajemen.tampilkan_semua_karyawan()
