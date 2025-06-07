c6b35ef824d12bc29b4ec4b33602e95fbcf658b7
revaprama <pramaulidiareva@gmail.com>
2025-06-07 16:38:56 +0700

    240441100131_REVA PRAMAULIDIA_AHMAD RIKHAN ARBA'I_MODUL 6 (#55)
    * 240441100131_REVA PRAMAULIDIA_AHMAD RIKHAN ARBA'I_MODUL 2
    
    * 240441100131_REVA PRAMAULIDIA_AHMAD RIKHAN ARBA'I_MODUL3
    
    * 240441100131_REVA PRAMAULIDIA_AHMAD RIKHAN ARBA'I_MODUL4
    
    * 240441100131_REVAPRAMAULIDIA_AHMADRIKHANARBA'I_MODUL5
    
    * 240441100131_REVA PRAMAULIDIA_AHMAD RIKHAN ARBA'I_MODUL 6

---
+++ modul-6/240441100131_REVAPRAMAULIDIA/soal1.py
@@ -0,0 +1,73 @@
+class BangunDatar:
+    def luas(self):
+        return 0
+
+class Persegi(BangunDatar):
+    def __init__(self, sisi):
+        self.sisi = sisi
+    
+    def luas(self):
+        return self.sisi * self.sisi
+
+class Lingkaran(BangunDatar):
+    def __init__(self, jari_jari):
+        self.jari_jari = jari_jari
+    
+    def luas(self):
+        pi = 3.14
+        return pi * self.jari_jari * self.jari_jari
+
+class Segitiga(BangunDatar):
+    def __init__(self, alas, tinggi):
+        self.alas = alas
+        self.tinggi = tinggi
+    
+    def luas(self):
+        return 0.5 * self.alas * self.tinggi
+
+def is_float_positif(string):
+    if string.count('.') == 1:
+        bagian1, bagian2 = string.split('.')
+        return bagian1.isdigit() and bagian2.isdigit()
+    return False
+
+def input_angka(pesan):
+    while True:
+        nilai = input(pesan)
+        if nilai.isdigit() or is_float_positif(nilai):
+            return float(nilai)
+        else:
+            print("Input harus angka positif")
+
+def luas_bangun(bangun):
+    return bangun.luas()
+
+while True:
+    print("\n Pilih Bangun Datar")
+    print("1. Persegi")
+    print("2. Lingkaran")
+    print("3. Segitiga")
+
+    pilihan = input("Masukkan pilihanmu (1/2/3): ")
+
+    if pilihan == "1":
+        sisi = input_angka("Masukkan panjang sisi: ")
+        persegi = Persegi(sisi)
+        hasil = luas_bangun(persegi)
+        print("Luas persegi =", hasil)
+
+    elif pilihan == "2":
+        jari_jari = input_angka("Masukkan jari-jari lingkaran: ")
+        lingkaran = Lingkaran(jari_jari)
+        hasil = luas_bangun(lingkaran)
+        print("Luas lingkaran =", hasil)
+
+    elif pilihan == "3":
+        alas = input_angka("Masukkan alas segitiga: ")
+        tinggi = input_angka("Masukkan tinggi segitiga: ")
+        segitiga = Segitiga(alas, tinggi)
+        hasil = luas_bangun(segitiga)
+        print("Luas segitiga =", hasil)
+        break
+    else:
+        print("Pilihan tidak valid")
