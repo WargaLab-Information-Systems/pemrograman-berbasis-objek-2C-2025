0edb1f719588f943ae5b61d73bcc5b5713e52764
Athinilistiqamah12 <athinilisti@gmail.com>
2025-06-07 16:06:59 +0700

    240441100129-A'THINIL ISTIQAMAH ALMUFIDAH-AHMAD RIKHAN ARBA'I-MODUL 6 (#56)
    * 240441100129-A'THINIL ISTIQAMAH ALMUFIDAH-AHMAD RIKHAN ARBA'I-MODUL 2
    
    * 240441100129-A'THINIL ISTIQAMAH ALMUFIDAH-AHMAD RIKHAN ARBA'I-MODUL 3
    
    * 240441100129-A'THINIL ISTIQAMAH ALMUFIDAH-AHMAD RIKHAN ARBA'I-MODUL 4
    
    * 240441100129-A'THINIL ISTIQAMAH ALMUFIDAH-AHMAD RIKHAN ARBA'I-MODUL 5
    
    * 240441100129-A'THINIL ISTIQAMAH ALMUFIDAH-AHMAD RIKHAN ARBA'I-MODUL 6

---
+++ modul-6/240441100129-A'THINIL ISTIQAMAH ALMUFIDAH/tugas1.py
@@ -0,0 +1,70 @@
+class BangunDatar:
+    def luas(self):
+        return 0
+
+
+class Persegi(BangunDatar):
+    def __init__(self, sisi):
+        self.sisi = sisi
+
+    def luas(self):
+        return self.sisi * self.sisi
+
+class Lingkaran(BangunDatar):
+    def __init__(self, jari_jari):
+        self.jari_jari = jari_jari
+
+    def luas(self):
+        phi = 3.14 
+        return phi * self.jari_jari * self.jari_jari
+
+class Segitiga(BangunDatar):
+    def __init__(self, alas, tinggi):
+        self.alas = alas
+        self.tinggi = tinggi
+
+    def luas(self):
+        return 0.5 * self.alas * self.tinggi
+
+
+def cetak_luas(bentuk):
+    print(f"Luas {bentuk.__class__.__name__}: {bentuk.luas():.2f}")
+
+
+while True:
+    print("\nPilih bangun datar:")
+    print("1. Persegi")
+    print("2. Lingkaran")
+    print("3. Segitiga")
+    print("4. Keluar")
+
+    pilih = input("Masukkan pilihan (1-4): ")
+
+    if pilih == 1:
+        try:
+            sisi = float(input("Masukkan panjang sisi: "))
+            bentuk = Persegi(sisi)
+            cetak_luas(bentuk)
+        except ValueError:
+            print("Input harus berupa angka.")
+    elif pilih == 2:
+        try:
+            r = float(input("Masukkan jari-jari lingkaran: "))
+            bentuk = Lingkaran(r)
+            cetak_luas(bentuk)
+        except ValueError:
+            print("Input harus berupa angka.")
+    elif pilih == 3:
+        try:
+            alas = float(input("Masukkan alas: "))
+            tinggi = float(input("Masukkan tinggi: "))
+            bentuk = Segitiga(alas, tinggi)
+            cetak_luas(bentuk)
+        except ValueError:
+            print("Input harus berupa angka.")
+    elif pilih == 4:
+        print("Terima kasih. Program selesai.")
+        break
+    else:
+        print("Pilihan tidak valid. Silakan coba lagi.")
+
